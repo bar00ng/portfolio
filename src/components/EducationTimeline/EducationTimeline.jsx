38f598bb332124f7f719d8378574945f3e7cf979
bar00ng <salamanderttf@gmail.com>
2023-02-09 15:26:38 +0700

    minor change on EducationTimeline

--- src/components/EducationTimeline/EducationTimeline.jsx
+++ src/components/EducationTimeline/EducationTimeline.jsx
@@ -10,22 +10,6 @@ import { education } from "../../data";
 export default function EducationTimeline() {
   return (
     <VerticalTimeline lineColor="white">
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        contentStyle={{ background: "#183153", color: "#fff" }}
-        dateClassName={"text-black"}
-        contentArrowStyle={{ borderRight: "7px solid  #183153" }}
-        date="2021 - present"
-        iconStyle={{ background: "#183153", color: "#fff" }}
-        icon={<School />}
-      >
-        <h3 className="vertical-timeline-element-title">LITHAN</h3>
-        <h5 className="vertical-timeline-element-subtitle">
-          Bachelor of Technology in Cloud Computing
-        </h5>
-        <p>Digital System Administration</p>
-      </VerticalTimelineElement>
-
       {education.map((item) => (
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
